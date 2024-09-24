77d13e7b4c7fb912fd3112a014fbef5e5f3c47d4
Kit Transue <src@kittransue.net>
2024-09-23 17:05:00 -0700

    Address concurrency issues.
    On macOS 15 and other platforms with Synchronization, use a Mutex
    to protect DeftLog settings. These targets can be compiled with Swift 6.
    
    Leave previous unsafe implementation on older platforms. (Swift 6 can
    be unsafely supported with a -D.)

--- Sources/DeftLog/DeftLog.swift
+++ Sources/DeftLog/DeftLog.swift
@@ -8,6 +8,13 @@
 //
 
 import Logging
+import Synchronization
+
+protocol LogFactory: Sendable {
+    func prefixLevels() -> [(prefix: String, level: Logger.Level)]
+    func setPrefixLevels(_ settings: [(prefix: String, level: Logger.Level)])
+    func logger(label: String) -> Logger
+}
 
 /// Centrally configure swift-log logLevel threshold based on logger label.
 public class DeftLog {
@@ -15,20 +22,60 @@ public class DeftLog {
     ///
     /// When a new `Logger` is requested through ``logger(label:)``, its label is checked against
     /// each of the prefixes in this array (in order). If there is a match, the level from the first
-    /// matching pair is applied to the Logger returned.
-    public static var settings: [(prefix: String, level: Logger.Level)] = []
-
+    /// matching pair is applied to the Logger before it is returned.
+    public static var settings: [(prefix: String, level: Logger.Level)] {
+        get {
+            singleton.prefixLevels()
+        }
+        set {
+            singleton.setPrefixLevels(newValue)
+        }
+    }
+    
     /// Create a new `Logger` and set its `logLevel`.
     ///
     /// After the new Logger is initialized, its label is checked against ``settings``. If there is a match, the
     /// corresponding log level will be applied to the new logger before it is returned.
     ///
     /// - Note: LogHandler assignment continues to use the same bootstrap mechanism as
-    /// if the Logger were initialized directly. Back-ends are wired as they would be without using DeftLog.
+    /// if the Logger were initialized directly. Back-ends continue to be wired as they would be without using DeftLog.
     public static func logger(label: String) -> Logger {
+        return singleton.logger(label: label)
+    }
+    
+    static let singleton: LogFactory = {
+#if swift(>=6.0) && !UNSAFEDEFTLOG
+        return MutexDeftLog()  // add "-D UNSAFEDEFTLOG" to run an unsafe log factory with Swift 6 and pre-Synchroniztion targets
+#else
+        if #available(macOS 15.0, iOS 18.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *) {
+            return MutexDeftLog()
+        }
+        return UnsafeDeftLog()
+#endif
+    }()
+    
+}
+
+@available(macOS 15.0, iOS 18.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
+internal final class MutexDeftLog: LogFactory, Sendable {
+    private let logSettings = Mutex<[(prefix: String, level: Logger.Level)]>([])
+    
+    internal func prefixLevels() -> [(prefix: String, level: Logger.Level)] {
+        return logSettings.withLock {
+            return $0
+        }
+    }
+    
+    internal func setPrefixLevels(_ settings: [(prefix: String, level: Logger.Level)]) {
+        logSettings.withLock {
+            $0 = settings
+        }
+    }
+
+    internal func logger(label: String) -> Logger {
         var logObject = Logger(label: label)
 
-        if let match = settings.first(where: { (prefix: String, level: Logger.Level) in
+        if let match = prefixLevels().first(where: { (prefix: String, level: Logger.Level) in
             label.starts(with: prefix)
         }) {
             logObject.logLevel = match.level
@@ -37,3 +84,40 @@ public class DeftLog {
         return logObject
     }
 }
+
+#if swift(<6.0) || UNSAFEDEFTLOG
+internal final class UnsafeDeftLog: Sendable, LogFactory {
+    // FIXME: serialize access on pre-Synchronization platforms
+    // Fix options are:
+    // - add SPM version of Atomics (complicated for the most-interesting macos14+swift6 case)
+    // - add Semaphore
+#if UNSAFEDEFTLOG
+#warning("Log factory is not concurrency safe on pre-Synchronization platforms.")
+    nonisolated(unsafe)  // say this is internally protected when it is not. But most access is reading, so hope for the best.
+    private var logSettings = [(prefix: String, level: Logger.Level)]([])
+#else
+    private var logSettings = [(prefix: String, level: Logger.Level)]([])
+#endif
+
+    
+    internal func prefixLevels() -> [(prefix: String, level: Logger.Level)] {
+        return logSettings
+    }
+    
+    internal func setPrefixLevels(_ settings: [(prefix: String, level: Logger.Level)]) {
+        logSettings = settings
+    }
+
+    internal func logger(label: String) -> Logger {
+        var logObject = Logger(label: label)
+
+        if let match = prefixLevels().first(where: { (prefix: String, level: Logger.Level) in
+            label.starts(with: prefix)
+        }) {
+            logObject.logLevel = match.level
+        }
+
+        return logObject
+    }
+}
+#endif
